9538abe0c47f242d6aa6f4bae37d61ceafa694c9
zhangb <1915987731@qq.com>
2019-06-19 19:25:54 +0800

    change dict

---
+++ dict/demo.py
@@ -0,0 +1,9 @@
+while True:
+    print("界面1")
+    cmd = input()
+    if cmd == 'in':
+        while True:
+            print("界面2")
+            cmdd = input()
+            if cmdd == 'q':
+                break
